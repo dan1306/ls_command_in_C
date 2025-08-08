9183112bee38d2ef860f803b38362368384f5626
daniel <dannysakyi@gmail.com>
2025-08-08 15:40:45 -0400

    partially done

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <getopt.h>
-#include <stdbool.h>
+#include <dirent.h>
+#include <sys/types.h>
+
 
 int main(){
-    prinf("ls command in C first init")
+
+    DIR *directory_stream = opendir("../");
+    if(directory_stream == NULL) {
+        perror("ERROR: unable to return a pointer to the provided directory stream. \n Check provided input and try again.\n");
+        return 0;
+    }
+
+    struct dirent *read_ptr = readdir(directory_stream);
+    while (read_ptr != NULL) {
+        printf("/%s\n",read_ptr->d_name);
+        read_ptr = readdir(directory_stream);
+    }
+    
+    return 0;
 }
